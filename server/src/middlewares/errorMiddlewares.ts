bc8a16f0013aeb2495f80914bd079035997628e1
Gis <centregeospatial@gmail.com>
2024-06-14 16:01:47 +0400

    Added: return type for error middleware

--- server/src/middlewares/errorMiddlewares.ts
+++ server/src/middlewares/errorMiddlewares.ts
@@ -4,7 +4,7 @@ import APIError from "../exceptions/apiError.js";
 import { writeToErrorFile } from "./morganMiddlewares.js";
 import MailError from "../exceptions/mailError.js";
 
-export default function (err: Error, req: Request, res: Response, next: NextFunction) {
+export default function (err: Error, req: Request, res: Response, next: NextFunction): Response<string, Record<string, any>> {
 	if (err instanceof APIError)
 		return res.status(err.status).json({ message: err.message, errors: err.errors });
 	if (err instanceof MailError) {
