61aba815cc35b7be1ba08f8085f00424598aa5af
Gis <centregeospatial@gmail.com>
2024-06-20 16:48:15 +0400

    Finished: client side

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Store from './store/store.ts'
+
+interface IStore {
+  store: Store;
+}
+
+export const store = new Store();
+
+export const Context = createContext<IStore>({
+  store,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Context.Provider value={ {store} }>
+      <App />
+    </Context.Provider>
   </React.StrictMode>,
 )
